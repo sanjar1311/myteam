ca8f07f04b04bfeef323de2c0c28daaa895c12a0
sanjar1311 <sanjarabduxamidov1311@gmail.com>
2020-11-12 14:02:53 +0500

    complate directors sections

--- main.js
+++ main.js
@@ -4,3 +4,18 @@ var siteHeader = document.querySelector('.site-header');
 burgerBtn.addEventListener('click', function(){
   siteHeader.classList.toggle('site-header--open')
 })
+
+
+/*   about-section */
+
+var directList = document.querySelector('.directors__list');
+
+var toggler = document.querySelector('.director__btn');
+
+toggler.addEventListener('click', function(){
+  directList.classList.toggle('directors__open');
+});
+
+toggler.addEventListener('click', function(){
+  toggler.classList.toggle('director__btn--close');
+});
